047f2ecff17d1fd28b9726337323629fe9679b2a
milomessin-fr <milo.messin@gmail.com>
2026-03-14 20:12:04 +0100

    update

--- src/config.py
+++ src/config.py
@@ -4,17 +4,17 @@
 
 
 #grains de sables tailles et quantités
-CUBE_X = 4
-CUBE_Y = 4
+CUBE_X = 4               #Taille des grains
+CUBE_Y = CUBE_X
 
-GRAINS_QUANTITY = 10000
+GRAINS_QUANTITY = 10000 # explicite
 
 
 
 
 # Moteur physique configurations
 GRAVITE = 800
-RIGIDITE = -0.16
+RIGIDITE = -0.16 #c le coef qui redistribue la vitesse après une collision
 
 
 
@@ -22,15 +22,15 @@ RIGIDITE = -0.16
 #Interface configurations
 WORLD_X = 40
 WORLD_Y = 40
-WORLD_WIDTH = 800
-WORLD_HEIGHT = 600
+WORLD_WIDTH = 800 #correspond à la largeur du monde physique
+WORLD_HEIGHT = 600 #correspond à la hauteur du monde physique   // pour changez la taille de l'écran changer ces 2 valeurs HEIGHT et WIDTH, tout va s'adapter nrml
 
 
 WINDOW_WIDTH = WORLD_X + WORLD_WIDTH + WORLD_X
 WINDOW_HEIGHT = WORLD_Y + WORLD_HEIGHT + WORLD_Y
 
 
-FACTORY = None
+FACTORY = None #Factory pour créer les sprites, les carrés .
 
 
 TC = CUBE_X #Unité de mesure des grains ça correspond à la taille d'un grain de sable
@@ -46,4 +46,4 @@ LIMITE_PLAFOND = WORLD_Y + 2
 
 # Delta time (temps entre chaque frame)
 FPS = 60
-DT = 0.16
+DT = 0.16 #1.0 / FPS 
